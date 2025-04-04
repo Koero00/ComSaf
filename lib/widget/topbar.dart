3145ff861dcb8d4e7682fdb304d6fc624f075c19
Arzumanian,Artem A.A. <artem.arzumanian@student.fontys.nl>
2025-04-04 12:16:08 +0200

    Home page | SOS Notification | Added a pop-up notification box that displayes the info of the Victim's SOS signal

--- lib/widget/topbar.dart
+++ lib/widget/topbar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';  // For formatting the time
 
 class TopSearchBar extends StatefulWidget {
   const TopSearchBar({super.key});
@@ -8,10 +9,19 @@ class TopSearchBar extends StatefulWidget {
 }
 
 class _TopSearchBarState extends State<TopSearchBar> {
+  bool showNotification = false;
+
+  String getCurrentTime() {
+    return DateFormat('HH:mm').format(DateTime.now());
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-          child: Padding(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Padding(
             padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.center,
@@ -27,14 +37,13 @@ class _TopSearchBarState extends State<TopSearchBar> {
                       BoxShadow(
                         color: const Color.fromARGB(255, 177, 177, 177),
                         blurRadius: 10,
-                        offset: Offset(0,5)
+                        offset: Offset(0, 5)
                       )
                     ]
                   ),
-                  
                   child: Row(
                     children: [
-                      Icon(Icons.search, color: const Color.fromARGB(255, 55, 57, 79),),
+                      Icon(Icons.search, color: const Color.fromARGB(255, 55, 57, 79)),
                       SizedBox(width: 12),
                       Expanded(
                         child: TextField(
@@ -48,9 +57,11 @@ class _TopSearchBarState extends State<TopSearchBar> {
                   ),
                 ),
                 ElevatedButton(
-                  onPressed: (){
-
-                  }, 
+                  onPressed: () {
+                    setState(() {
+                      showNotification = !showNotification;
+                    });
+                  },
                   style: ElevatedButton.styleFrom(
                     shape: CircleBorder(),
                     padding: EdgeInsets.all(20),
@@ -62,11 +73,73 @@ class _TopSearchBarState extends State<TopSearchBar> {
                     Icons.notifications,
                     color: Color.fromARGB(255, 55, 57, 79),
                     size: 28,
-                    ),
+                  ),
                 )
               ],
             ),   
           ),
-        );
+          
+          // SOS Notification Box
+          AnimatedContainer(
+            duration: Duration(milliseconds: 300),
+            height: showNotification ? 90 : 0,  // Adjusted the height for space
+            width: MediaQuery.of(context).size.width,
+            margin: EdgeInsets.symmetric(horizontal: 16),
+            decoration: BoxDecoration(
+              color: Color(0xFFFF5252),
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: showNotification 
+                ? Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: CircleAvatar(
+                          radius: 35, // Profile picture size
+                          backgroundImage: AssetImage('assets/profile_pic1.png'),
+                        ),
+                      ),
+                      Expanded(
+                        child: Align(
+                          alignment: Alignment.centerLeft,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              SizedBox(height: 10),  // Distance from the top
+                              Text(
+                                "S.O.S Emergency",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              SizedBox(height: 5),
+                              Text(
+                                "Asthma attack",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 14,
+                                ),
+                              ),
+                              SizedBox(height: 5),
+                              Text(
+                                getCurrentTime(),
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 14,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  )
+                : null,
+          ),
+        ],
+      ),
+    );
   }
 }
